43c6271b5c5f506b3606ee7c482b946684a4dae9
kubicue <3x2media@gmail.com>
2015-10-31 01:26:22 -0700

    working on tracking view and controller

--- velox_app/app/controllers/tracking_controller.rb
+++ velox_app/app/controllers/tracking_controller.rb
@@ -3,7 +3,9 @@ class TrackingController < ApplicationController
   end
   def create
     ## Creating a Tracker
-    
+    # track code:EZ4000000004
+    #carrier:"UPS" "USPS"
+
   tracker = EasyPost::Tracker.create({
   tracking_code: "EZ4000000004",
   carrier: "UPS"
